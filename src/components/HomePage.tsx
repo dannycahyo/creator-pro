d71bf26a8b370881e0050cb904f27ba0be6c89c8
dannycahyo <dannydwicahyono@gmail.com>
2024-07-31 22:52:57 +0700

    refactor: Update HomePage component to use Chakra UI Center and improve layout

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,19 +1,23 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading } from "@chakra-ui/react";
 import Layout from "@src/components/Layout";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout>
-      <Heading size="lg">Welcome to Creator PRO</Heading>
-      <Box p={12}>
-        <Image
-          alt="Content Creator Ilustration"
-          src="/content_creator.svg"
-          width={300}
-          height={300}
-        />
-      </Box>
+      <Center>
+        <Box>
+          <Heading size="lg">Welcome to Creator PRO</Heading>
+          <Box mt={10}>
+            <Image
+              alt="Content Creator Ilustration"
+              src="/content_creator.svg"
+              width={300}
+              height={300}
+            />
+          </Box>
+        </Box>
+      </Center>
     </Layout>
   );
 }
