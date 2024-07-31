1d6c4f91d32bb00bd1e6b86f8ba93a581ad93754
dannycahyo <dannydwicahyono@gmail.com>
2024-07-31 22:53:47 +0700

    refactor: Update MarkdownComponent to use Chakra UI Heading components with margin-bottom

--- src/components/MarkdownComponent.tsx
+++ src/components/MarkdownComponent.tsx
@@ -20,12 +20,21 @@ const MarkdownComponent: React.FC<MarkdownComponentProps> = ({
   return (
     <ReactMarkdown
       components={{
-        h1: (props) => <Heading as="h1" size="2xl" {...props} />,
-        h2: (props) => <Heading as="h2" size="xl" {...props} />,
-        h3: (props) => <Heading as="h3" size="lg" {...props} />,
-        h4: (props) => <Heading as="h4" size="md" {...props} />,
-        h5: (props) => <Heading as="h5" size="sm" {...props} />,
-        h6: (props) => <Heading as="h6" size="xs" {...props} />,
+        h1: (props) => (
+          <Heading mb={4} as="h1" size="xl" {...props} />
+        ),
+        h2: (props) => (
+          <Heading mb={4} as="h2" size="lg" {...props} />
+        ),
+        h3: (props) => (
+          <Heading mb={4} as="h3" size="md" {...props} />
+        ),
+        h4: (props) => (
+          <Heading mb={4} as="h4" size="sm" {...props} />
+        ),
+        h5: (props) => (
+          <Heading mb={4} as="h5" size="xs" {...props} />
+        ),
         p: (props) => <Text mb={2} {...props} />,
         a: (props) => <Link color="teal.500" {...props} />,
         li: (props) => <ListItem {...props} />,
